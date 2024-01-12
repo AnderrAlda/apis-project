c86c29488f4b6550354fcd04c2b56bb522aa8ad2
JuanAzula <juanazula3@gmail.com>
2024-01-12 10:03:19 +0100

    cats-section

---
+++ cats.js
@@ -0,0 +1,26 @@
+const catsTitle = document.getElementById("catsTitle");
+
+catsTitle.addEventListener("click", async function () {
+    const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=10")
+    const imagesUrl = await response.json()
+
+    imagesUrl.forEach(image => {
+
+        const catsContainer = document.getElementById("catsContainer");
+
+        const catsImage = document.createElement("img");
+        catsImage.src = image.url;
+
+        catsContainer.classList.add("cats-container");
+        catsImage.classList.add("cats-images");
+
+        catsContainer.appendChild(catsImage);
+
+
+    });
+
+})
+
+// const respuesta = await fetch(URL_PRODUCTOS); //fetch para recibir los productos
+// const productos = await respuesta.json();   //nueva variable para almacenar los productos en formato json
+// return productos;
